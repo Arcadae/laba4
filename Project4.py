d68de1f8e0a5f92a6795f3307b633f15725594d2
Arcadae <161068051+Arcadae@users.noreply.github.com>
2024-03-23 11:48:32 +0400

    Add files via upload

---
+++ Project4.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_matrix(n):
+    return np.random.randint(-10, 10, size=(n, n))
+
+def split_matrix(matrix):
+    n = len(matrix)
+    half = n // 2
+    return matrix[:half, :half], matrix[:half, half:], matrix[half:, :half], matrix[half:, half:]
+
+def count_even_cols(matrix,K):
+    return np.sum(matrix[:, ::2] > K)
+
+def count_odd_rows_product(matrix):
+    return np.prod(matrix[1::2, :])
+
+def swap_matrices_symmetrically(matrix1, matrix2):
+    n = len(matrix1)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if i + j != n - 1:
+                matrix1[i, j], matrix2[n - j - 1, n - i - 1] = matrix2[n - j - 1, n - i - 1], matrix1[i, j]
+
+def swap_matrices(matrix1, matrix2):
+    matrix1[:], matrix2[:] = matrix2[:], matrix1[:]
+
+def main():
+    try:
+        K = int(input("Введите число K: "))
+        N = int(input("Введите число N: "))
+    except ValueError:
+        print('Матрица не может быть размером из нецелого числа')
+        return
+
+    A = generate_matrix(N)
+    print("Матрица A:")
+    print(A)
+
+    B, C, D, E = split_matrix(A)
+
+    F = np.vstack((np.hstack((B, C)), np.hstack((D, E))))
+    print("Матрица F:")
+    print(F)
+
+    if count_even_cols(F,K) > count_odd_rows_product(F):
+        swap_matrices_symmetrically(C, E)
+    else:
+        swap_matrices(C, B)
+
+    print("Матрица C после обмена:")
+    print(C)
+    print("Матрица E после обмена:")
+    print(E)
+
+    det_A = np.linalg.det(A)
+    sum_diag_F = np.sum(np.diag(F))
+
+    G = np.tril(A, -1)
+    print("Матрица G:")
+    print(G)
+
+    if det_A > sum_diag_F:
+        result = A @ A.T - K * np.linalg.inv(F)
+    else:
+        result = (np.linalg.inv(A) + G - F.T) * K
+
+    print("Результирующая матрица:")
+    print(result)
+
+    plt.matshow(A)
+    plt.title('Матрица A')
+    plt.colorbar()
+    plt.show()
+
+    plt.matshow(F)
+    plt.title('Матрица F')
+    plt.colorbar()
+    plt.show()
+
+    plt.matshow(result)
+    plt.title('Результат выражения')
+    plt.colorbar()
+    plt.show()
+    
+if __name__=="__main__":
+    main()
